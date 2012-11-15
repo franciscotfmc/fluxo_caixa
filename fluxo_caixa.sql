a1056c9879cc7b3e359b0ae571f9efac827ea980
Francisco <f.thiene@gmail.com>
2012-11-15 19:02:36 -0200

    sql for create the tables

--- fluxo_caixa.sql
+++ fluxo_caixa.sql
@@ -9,3 +9,22 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `senha` varchar(25) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=23;
+
+CREATE TABLE conta (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  conta_id INT(11) NULL,
+  numero VARCHAR(100) NOT NULL,
+  flag_tipo SMALLINT(1) UNSIGNED NOT NULL,
+  PRIMARY KEY(id),
+  INDEX fk_conta_conta(conta_id)
+);
+
+CREATE TABLE fluxo (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  conta_id INT(11) NOT NULL,
+  descricao VARCHAR(255) NOT NULL,
+  dt_fluxo DATE NOT NULL,
+  valor FLOAT(8,2) NOT NULL,
+  PRIMARY KEY(id),
+  INDEX fk_fluxo_conta(conta_id)
+);
