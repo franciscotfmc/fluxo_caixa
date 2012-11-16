9d3f5f81144e0c4289fa88e2022f3571a97eaf79
Maurício Batista <eddloschi@gmail.com>
2012-11-16 14:20:35 -0200

    Use MVC in Conta controller

--- app/controller/Conta.js
+++ app/controller/Conta.js
@@ -126,50 +126,21 @@ Ext.define('FM.controller.Conta', {
             var record = form.getRecord(),
                 values = form.getValues();
             if (record)
-			{
+            {
                 if(record.data['id'])
-				{
-					Ext.Ajax.request ({
-						scope	: this,
-						url		: 'php/contas.php?acao=update', //arquivo que contém o método a utilizar
-						params	: {
-						    'id' : id,
-						    'conta_id' : values.conta_id,
-						    'nome' : values.nome,
-						    'flag_tipo' : values.flag_tipo,
-						},
-
-						success: function(r){
-
-							//Se tudo OK, pegamos a resposta que é um JSON e decodificamos para um objeto
-							var obj = Ext.decode(r.responseText);
-							//Verificamos se obtivemos sucesso na ação
-							if(obj.success)
-							{
-								Ext.Msg.alert('Sucesso', obj.message);
-							}
-							else
-							{
-								Ext.Msg.alert('Erro', obj.message); //exibe a mensagem
-							}
-						},
-
-						failure: function(){ // se houve algum erro ao submeter o formulário
-								Ext.Msg.alert('Erro', 'Erro na comunicação com o servidor.'); //exibe a mensagem
-						}
-					});
+                {
+                    record.set(values);
                 }
             }
-			else
-			{
-                var record = Ext.create('FM.model.Conta');
+            else
+            {
+                record = Ext.create('FM.model.Conta');
                 record.set(values);
                 this.getContaList().store.add(record);
-				this.getContaList().store.sync();
             }
 
             win.close();
-			this.getContaList().store.load();
+            this.getContaList().store.sync();
         }else{
             Ext.ux.Msg.flash({
                 msg: 'Há campos preenchidos incorretamente',
